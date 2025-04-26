bb24f2237f52fa462876845ef1be49a8297496de
Firstrex13 <seregka001@gmail.com>
2025-04-26 20:13:39 +0300

    SecondCommit

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private void Update()
+    {
+        transform.Translate(0, 0, _speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    [SerializeField] private float _rotationSpeed;
+    private void Update()
+    {
+        transform.Rotate(0, _rotationSpeed * Time.deltaTime, 0, Space.Self);
+    }
+}
