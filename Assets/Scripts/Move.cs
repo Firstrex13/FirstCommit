97e51bafdb22939ff74941c03a6fff8818613a1b
Firstrex13 <seregka001@gmail.com>
2025-04-26 20:25:40 +0300

    ThirdCommit

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     [SerializeField] private float _speed;
+
     private void Update()
     {
         transform.Translate(0, 0, _speed * Time.deltaTime);
--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Rotation : MonoBehaviour
 {
     [SerializeField] private float _rotationSpeed;
+
     private void Update()
     {
         transform.Rotate(0, _rotationSpeed * Time.deltaTime, 0, Space.Self);
